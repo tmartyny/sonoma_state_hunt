d1fce13cb3d9160390d5adef5420a9da75fb935d
Teresa Martyny <teresamartyny@gmail.com>
2015-09-09 22:15:56 -0700

    nest submissions and challenges within user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :submissions, only: [:new, :index, :create]
+    resources :challenges, only: [:show]
+  end
+
+  
   root to: 'visitors#index'
+
+  # User Auth
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
