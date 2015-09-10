649ced05123b4a28710ae38a2daa1f4fc19dbb16
Teresa Martyny <teresamartyny@gmail.com>
2015-09-09 22:24:21 -0700

    add seeds for challenges. once mvp is complete, will come back to add admin module allowing crud for challenges.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,135 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Challenge.create(
+  video_id: "MuI7lY9Y-rc",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/VC%20Maestrapeace%20mural_zpspj0a0z02.jpg",
+  name: "Belonging and the Valencia Corridor",
+  direction: "Head down 18th Street!",
+  points: 10,
+  clue: "Find the largest mural in the Mission, on two sides of this center for women's organizing, support, and community for the last three decades. Take a picture with the image of black lesbian poet Audre Lorde in the background. What is the name of the mural, and when was it completed? What famous AIDS activist organization is represented on the mural? Name the two places it is referenced. Take a picture of Cherrie Moraga’s name on the mural.",
+  lat: 37.76158269,
+  lon: -122.42258068)
+Challenge.create(
+  video_id: "AZwQpivHVSE",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/VC%20DM_J_Cleland_DM_Crowd_zpss9zucetv.jpg",
+  name: "Dyke March",
+  direction: "Find the nearby famous park",
+  points: 10,
+  clue: "Since 1993, this has been the launching site for the Dyke March, which has inspired similar demonstrations in nearly 20 other cities around the world, including the more recent Trans March. Take a picture of a real, live lesbian and/or transperson in this park.",
+  lat: 37.76076863,
+  lon: -122.42661148)
+Challenge.create(
+  video_id: "_WqiTnbZ0vo",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CVH%20Mauds%20Study_zpshgyz3itd.jpg",
+  name: "Cole Valley",
+  direction: "HEAD OVER THE HILL TO COLE VALLEY/THE HAIGHT!",
+  points: 10,
+  clue: "Find the bar in Cole Valley named after a James Joyce novel.  From 1966-1989, this was the most popular lesbian bar in the city.  Name it and take a picture of your whole team in front of it. What is the name of the documentary about this bar? ",
+  lat: 37.76530787,
+  lon: -122.4500055)
+Challenge.create(
+  video_id: "Nttdjtthfhs",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CVH%20Circus%20at%20Ibeam%201978_zpsn0eq5zck.jpg",
+  name: "I-Beam bar",
+  direction: "On Haight between Cole and Shrader",
+  points: 10,
+  clue: "Find the retail store that sounds like a gay flower.  This was once was the I-Beam bar. Owned by Sanford Kellerman, a former astronomer, it’s popular early 1980s Sunday Tea Dance was followed by live punk shows on Monday nights, bringing together queers of all stripes with the thriving punk scene. Take a picture of a team member in a really hardcore pose in front.",
+  lat: 37.76957065,
+  lon: -122.45108911)
+Challenge.create(
+  video_id: "RseMaNDVnbs",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CC%20CRH%20Arrest_zps4s2pyg8a.gif",
+  name: "Politics and Protests in Civic Center",
+  direction: "Head Over to City Hall",
+  points: 10,
+  clue: "Near Civic Center is California Hall, where the Council on Religion and the Homosexual held their New Year’s Eve, 1965 dance where police went too far, arresting lawyers and heterosexual ministers‘ wives.  It set off a widespread criticism of arbitrary police harassment of gays and rallied support for the rights of gay people to assemble.  California Hall is on a street named after an aggressively expansionist 19th-century U.S. president, and if you find what occupies the building today, you’ll really be cooking and could get the paper to prove it. Take a picture of a person being led out of California Hall as if they were being arrested.",
+  lat: 37.78237657,
+  lon: -122.41917555)
+Challenge.create(
+  video_id: "VSbEaMItn3A",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CC%20dr_david_lourea_letter_1984%20Bathhouse%20Closures_zpsor8fxgtk.jpg",
+  name: "Auto Immune Deficiency Syndrome",
+  direction: "Explore the Civic Center",
+  points: 10,
+  clue: "On April 9, 1984, a prominent city agency took the drastic, controversial step of ordering gay bathhouses closed down, ostensibly to prevent the spread of AIDS for the good of public health.  Find the Civic Center building where this agency is, which was also the site of protests related to the bathhouse closures, and take a picture of the sign on the building containing all of the letters in the acronym for Auto Immune Deficiency Syndrome.",
+  lat: 37.77837272,
+  lon: -122.41837848)
+Challenge.create(
+  video_id: "Kucy_Z9Pnfo",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CC%20Hormel%20Center%20of%20SFPL_zpshgzrplau.jpg",
+  name: "And the Band Played On",
+  direction: "Inside the building with free books",
+  points: 10,
+  clue: "Find a room dedicated to lesbian, gay, bisexual, and transgender history, literature, and culture. Go there and take a picture of the section of the mural that includes the author of And the Band Played On, the author of Orlando, and the openly gay black man who was the main architect of the 1963 March on Washington for civil rights.<br>To which country did the main donor to this room become the first openly gay U.S. ambassador?<br>Take a picture of the famous four-letter convenience food partially responsible for this person’s family fortune.<br>Get an informational pamphlet for the room.",
+  lat: 37.77901104,
+  lon: -122.41596311)
+Challenge.create(
+  video_id: "LVPN1ayk878",
+  photo_url: "http://s723.photobucket.com/user/misterromesburg/media/CC%20Gay_in_military_rally_SF_1966_zpsza6wvwwb.jpg.html?sort=3&o=30",
+  name: "Armed Forces",
+  direction: "",
+  points: 5,
+  clue: "A nearby tall national office building named for San Francisco’s U.S. Representative from the 1960s-1980s was the site of the May 21, 1966 protest of exclusion of homosexuals from the Armed Forces. Take a picture of a team member pole dancing on the flag pole of this building.<br>Take a picture with team members holding up signs indicating all four parts of the military’s recently repealed policy to lesbian and gay servicemembers.",
+  lat: 37.78166452,
+  lon: -122.41821561)
+Challenge.create(
+  video_id: "8Ds7RQPS4c8",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CC%20Del%20and%20Phylis%20get%20hitched%202004_zps0fkorvg9.jpg",
+  name: "Going to the Chapel",
+  direction: "",
+  points: 5,
+  clue: "Take a picture of yourselves wearing makeshift wedding veils on the steps of the location that many gay couples were legally married in Spring 2004 and again from June-November 2008. The same building were same-sex couples got married was also the building where Dan White murdered Harvey Milk. What is the first and last name of the other person Dan White murdered that day? Of what crime was Dan White found guilty?",
+  lat: 37.77927861,
+  lon: -122.41870392)
+Challenge.create(
+  video_id: "8Ds7RQPS4c8",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/C%20Harvey%20Milk_zps4eedp2hi.jpg",
+  name: "Assassination",
+  direction: "",
+  points: 5,
+  clue: "The same building were same-sex couples got married was also the building where Dan White murdered Harvey Milk. What is the first and last name of the other person Dan White murdered that day? Of what crime was Dan White found guilty?",
+  lat: 37.77927861,
+  lon: -122.41870392)
+Challenge.create(
+  video_id: "GRNk-gYqNUI",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/CC%20DOBConventionProgram_zpsjagaiv3b.png",
+  name: "Daughters of Bilitis",
+  direction: "Find the Hotel Whitcomb Hotel on Market St. between 10th and 7th.",
+  points: 5,
+  clue: "In 1960, this where the first US lesbian political organization, the Daughters of Bilitis, held their first national convention.  After the 1906 quake, it had also served as a temporary City Hall and jail. In the 1990s and early 2000s, it was also home to the International Bear Rendezvous. Take a picture of one team member acting like a bear and another acting like a political lesbian fending him off.",
+  lat: 37.77811682,
+  lon: -122.4152346)
+Challenge.create(
+  video_id: "TgHszhfXAZ4",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/PHF%20Lions%20Pub_zpsvlgzsyut.jpg",
+  name: "Pacific Heights",
+  direction: "On Divisadero somewhere between California and Clay",
+  points: 5,
+  clue: "Serving the swanky gay sweater set starting in 1969, this corner bar was the Pacific Heights’ go-to homo hangout through the next three decades.  Now, sadly, it is a straight ex-frat-boy bar.  Have a passerby take a picture the stained glass window representing the bar’s name, with the whole team acting like lions. Grrl!",
+  lat: 37.78892496,
+  lon: -122.4404526)
+Challenge.create(
+  video_id: "TgHszhfXAZ4",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/PHF%20Lions%20Pub_zpsvlgzsyut.jpg",
+  name: "The Big Glass",
+  direction: "On Fillmore between Post and Bush",
+  points: 5,
+  clue: "There are two amazingly queer historic sites that now have no resemblance to their earlier incarnations.  First, find the establishment on a corner that has a triangle made of squares as its logo. From 1964 through 1968, when this yuppie haven was still an African American neighborhood, this was the location of the Big Glass, the first black-owned, black-oriented gay bar in San Francisco. Take a picture of purple squares on the current establishment’s logo.",
+  lat: 37.78617251,
+  lon: -122.4333844)
+Challenge.create(
+  video_id: "TgHszhfXAZ4",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/PHF%20orval-hixon-bothwell-browne-1920_zpsbd0lzzvr.jpg",
+  name: "Bothwell Browne School of Dancing and Stage Culture",
+  direction: "Now, walk to the other end of that block of Fillmore",
+  points: 5,
+  clue: "Find the big windows on a brick building.  In the first decade of the 20th century, this was the door to the Bothwell Browne School of Dancing and Stage Culture.  Take a photo of a team member instructing another team member in such matters in front of the appropriate window. What was Bothwell Browne’s more famous occupation?",
+  lat: 37.78676995,
+  lon: -122.43355284)
+Challenge.create(
+  video_id: "UCxFDf7VvOc",
+  photo_url: "http://i723.photobucket.com/albums/ww233/misterromesburg/PHF%20orval-hixon-bothwell-browne-1920_zpsbd0lzzvr.jpg",
+  name: "Club Alabam",
+  direction: "Post near Fillmore",
+  points: 5,
+  clue: "In May 1951, jazz hotspot Club Alabam was one of 21 bars that the US Armed Forces Disciplinary Control Board investigated.  Military Police visited the joint over 20 times (just to be sure?) before concluding that the place has not just female prostitutes but “certain homosexual elements.”  They placed it “off limits and out of bounds” for 90 days.  The memos servicemembers received regarding banned places provided an early gay guide to newcomers to the San Francisco scene.  Find its former site on Post near Fillmore where the current establishment shares its name with a famous San Francisco landmark.  Take a picture of the entire team saluting with a limp wrist, Mary.",
+  lat: 37.7853787,
+  lon: -122.43213996)
