d4f5dbe1372744c1635ab5ae8d6249e63a461a43
Teresa Martyny <teresamartyny@gmail.com>
2015-09-10 13:33:29 -0700

    render show without layout for ajax request

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -6,5 +6,6 @@ class ChallengesController < ApplicationController
     @user = User.find(params[:user_id])
     @challenge = Challenge.find(params[:id])
     @total_points = User.total_points(current_user)
+    render 'show', layout: false
   end
 end
