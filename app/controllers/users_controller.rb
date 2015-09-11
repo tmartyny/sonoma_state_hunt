c22efa6dc1cb6f08da6a3593af311981e404a2b1
Teresa Martyny <teresamartyny@gmail.com>
2015-09-11 14:57:50 -0700

    add has_submissions and hunt_complete? methods to simplify conditional rendering on user show page. change pluck to where. that only worked because I'm the only user right now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user?, :except => [:index]
 
-  def index
-    @users = User.all
-  end
-
   def show
-    @user = User.find(params[:id])
     @challenge = Challenge.first
     @total_points = User.total_points(current_user)
     @total_submissions = User.total_submissions(current_user)
     @next_challenge = User.next_challenge(current_user)
+    @has_submissions = User.has_submissions?(current_user)
+    @hunt_complete = User.hunt_complete?(current_user)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.has_submissions?(user)
+    Submission.where(user_id: user.id).any?
+  end
+
   def self.total_points(user)
     if Submission.where(user_id: user.id).length > 0
       Challenge.find(Submission
@@ -23,7 +27,18 @@ class User < ActiveRecord::Base
   end
 
   def self.total_submissions(user)
-    Submission.pluck(:user_id).length
+    Submission.where(user_id: user.id).length
+  end
+
+  def self.last_submission(user)
+    return if !User.has_submissions?(user)
+    Submission.where(user_id: user.id)
+              .last
+              .challenge_id
+  end
+
+  def self.hunt_complete?(user)
+    Challenge.final_challenge_id == User.last_submission(user)
   end
 
   def self.next_challenge(user)
