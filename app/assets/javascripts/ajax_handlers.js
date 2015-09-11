42f37c2aafb13f2f21de58379a45a86d373b5388
Teresa Martyny <teresamartyny@gmail.com>
2015-09-10 21:46:55 -0700

    remove old sinatra style ajax placeholder from sinatra version of project. since the ajax calls for rendering challenge and posting the submission were the same except for link vs form, I temporarily made one ajax call that works for both and made the render challenge a button_to. This is not ideal since forms should not make get requests. I will come back to this shortly and resolve in a way that is dry while maintaining proper functioning. The controllers for the ajax calls are not using respond_to at the moment. I will integrate that functionality next.

--- app/assets/javascripts/ajax_handlers.js
+++ app/assets/javascripts/ajax_handlers.js
@@ -1,41 +1,13 @@
 $(function() {
-  renderChallenge();
-  // createSubmissionHandler(); //submit form for user solution
+  ajaxHandler(); //submit form for user solution
 });
 
-var createSubmissionHandler = function() {
-  $('.container').on('submit', '#form_submission', function(event){
-    event.preventDefault();
-    var form_data = $(this).serialize();
-    var request = $.ajax({
-      url: this.action,
-      type: this.method,
-      data: form_data
-    });
-
-    request.done(function(response) {
-      successMessage = "<span class='success'><p>Excellent!</p><p>Watch the video on the left to learn more.</p><p>Click 'Next Clue' to move along to the next clue. You're on a roll!</p><p>Don't forget to keep an eye on the clock.</p><p>You have ## landmarks left to find.</p></span>"
-      $(".challenge_body").empty(); //remove current contents of screen
-      $(".challenge_body").append(data); //display new challenge
-
-      // revist options below once videos are added to system
-      // $("#form_submission").remove(); //remove the submission form
-      // $("#challenge_media").append(response); //add the video and next button
-      // $("#challenge_clue").append(successMessage); //add the success message below clue
-    });
-
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-      var error = errorThrown;
-      console.log("fail" + error);
-    });
-  });
-};
-
-var renderChallenge = function() {
-  $('.container').on('ajax:success', 'a[data-remote=true]', function(event, data, status, xhr){
+var ajaxHandler = function() {
+  $('.container').on('ajax:success', 'form[data-remote=true]', function(event, data, status, xhr){
     $(".challenge_body").empty(); //remove current contents of screen
     $(".challenge_body").append(data); //display new challenge
+  }).bind("ajax:error", function(e, xhr, status, error) {
+    console.log("Ajax Error!");
   });
 };
-
 
