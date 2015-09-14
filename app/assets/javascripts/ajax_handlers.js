28ff3b3a11fb4c83516ee706b323bad8dbe2cd2d
Teresa Martyny <teresamartyny@gmail.com>
2015-09-14 11:25:29 -0700

    add testing time option commented out. add finish option to end the hunt by gutting the challenge div and displaying a warning that the hunt is over.

--- app/assets/javascripts/ajax_handlers.js
+++ app/assets/javascripts/ajax_handlers.js
@@ -10,4 +10,3 @@ var ajaxHandler = function() {
     console.log("Ajax Error!");
   });
 };
-
--- vendor/assets/javascripts/clock.js
+++ vendor/assets/javascripts/clock.js
@@ -1,10 +1,16 @@
 var startClock = function() {
   var startTime = Date.now();
+  // var endTime = startTime + 1080; //for testing purposes
   var endTime = startTime + 10800000;
   var countdownDate = new Date(endTime);
-  $('#clock').countdown(countdownDate, function(event) {
-  $(this).html(event.strftime('%H:%M:%S'));
-  });
+  $('#clock')
+    .countdown(countdownDate, function(event) {
+      $(this).html(event.strftime('%H:%M:%S'));
+    })
+    .on('finish.countdown', function(event) {
+      $(".challenge_body").html("<h4 class='text-warning'>Time is up! If you are not back at the rendezvous point at this time you will be disqualified.</h4>");
+      $('#clock').addClass('disabled');
+    });
 }
 
 $(document).ready(startClock);
