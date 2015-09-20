0fe89a18268fff16bde2b1a23f9b84ebebb3bbbf
Teresa Martyny <teresamartyny@gmail.com>
2015-09-20 14:25:47 -0700

    add route to update a submission

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :submissions, only: [:new, :index, :create]
+    resources :submissions, only: [:new, :index, :create, :update]
     resources :challenges, only: [:show]
   end
 
