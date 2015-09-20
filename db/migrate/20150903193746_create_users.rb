909d391fb66c823eb7f5affa65e30bf8d49b1ddf
Teresa Martyny <teresamartyny@gmail.com>
2015-09-20 14:29:05 -0700

    add to migration email, admin and current_student. I'm reseeding frequently at this stage of testing so it's ok to not create a new migration

--- db/migrate/20150903193746_create_users.rb
+++ db/migrate/20150903193746_create_users.rb
@@ -2,8 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
       t.string :provider
       t.string :uid
+      t.boolean :admin,  default: false
+      t.boolean :current_student, default: false
 
       t.timestamps null: false
     end
